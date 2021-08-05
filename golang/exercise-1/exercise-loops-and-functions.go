59211038c86298a4c3ba40c30e5b3db1a83e948e
Itu-tree <toutounode@gmail.com>
2021-08-05 09:30:34 +0900

    implement sqrt function

--- golang/exercise-1/exercise-loops-and-functions.go
+++ golang/exercise-1/exercise-loops-and-functions.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+
+
 func Sqrt(x float64) float64 {
+	z := float64(1)
+	d := 1.0
+	for math.Abs(d) > 1e-10 {
+		d = (z*z - x) / (2 * z)
+		z -= d
+	}
+	return z
 }
 
 func main() {
